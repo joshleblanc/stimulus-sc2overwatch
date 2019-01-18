90685f23b2084040078ebbf368bb44ed383e8bfd
Josh LeBlanc <jleblanc@omics.com>
2019-01-18 16:04:27 -0400

    Moved loader toggle to its own function

--- app/javascript/controllers/upload_modal_controller.js
+++ app/javascript/controllers/upload_modal_controller.js
@@ -5,23 +5,22 @@ export default class extends Controller {
 
     static targets = [ "modal", "collapse", "dropzone", "placeholder", "loader", "openButton" ];
 
-    connect() {
-    }
     initialize() {
         this.dialog = new dialog.MDCDialog(this.modalTarget);
         this.dropzone = new Dropzone(this.dropzoneTarget, {
             previewTemplate: "<div></div>"
         });
         this.dropzone.on('addedfile', () => {
-            this.placeholderTarget.classList.toggle('upload-state--current');
-            this.loaderTarget.classList.toggle('upload-state--current');
+            this.toggleLoader();
         });
         this.dropzone.on('success', (a, b) => {
-            this.placeholderTarget.classList.toggle('upload-state--current');
-            this.loaderTarget.classList.toggle('upload-state--current');
-            this.collapse.open(1);
-            console.log(a,b);
+            this.toggleLoader();
         });
+    }
+
+    toggleLoader() {
+        this.placeholderTarget.classList.toggle('upload-state--current');
+        this.loaderTarget.classList.toggle('upload-state--current');
     }
 
     openModal() {
