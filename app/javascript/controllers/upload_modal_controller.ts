0ec782063cfc45ee13f5c0d025207d84effb4806
Josh LeBlanc <jleblanc@omics.com>
2019-01-18 22:24:34 -0400

    wip

--- app/javascript/controllers/upload_modal_controller.ts
+++ app/javascript/controllers/upload_modal_controller.ts
@@ -1,37 +1,69 @@
 import { Controller } from 'stimulus';
 import { dialog } from 'material-components-web/index';
-import { Dropzone } from 'dropzone';
+import Dropzone from 'dropzone';
 
 export default class extends Controller {
-    static targets = [ "modal", "collapse", "dropzone", "placeholder", "loader", "openButton" ];
+    static targets = [ "modal", "collapse", "dropzone", "openButton", "step", "replayInput", "uploadForm" ];
     readonly modalTarget!: Element;
     readonly dropzoneTarget!: Element;
-    readonly placeholderTarget!: Element;
-    readonly loaderTarget!: Element;
+    readonly stepTargets!: Array<Element>;
+    readonly replayInputTarget!: HTMLInputElement;
+    readonly uploadFormTarget!: HTMLFormElement;
     dialog: dialog.MDCDialog;
     dropzone: Dropzone;
 
+
     initialize() {
+        this.showCurrentStep();
         this.dialog = new dialog.MDCDialog(this.modalTarget);
-        this.dropzone = new Dropzone(this.dropzoneTarget, {
-            previewTemplate: "<div></div>"
-        });
-        this.dropzone.on('addedfile', () => {
-            this.toggleLoader();
-        });
-        this.dropzone.on('success', (a, b) => {
-            this.toggleLoader();
-        });
+        // this.dropzone = new Dropzone(this.dropzoneTarget, {
+        //     previewTemplate: "<div></div>"
+        // });
+        // this.dropzone.on('addedfile', () => {
+        //     this.nextStep();
+        // });
+        // this.dropzone.on('success', (a, b) => {
+        //     console.log(a,b);
+        //     this.nextStep();
+        // });
     }
 
-    toggleLoader() {
-        this.placeholderTarget.classList.toggle('upload-state--current');
-        this.loaderTarget.classList.toggle('upload-state--current');
+    showCurrentStep() {
+        console.log(this.step);
+        this.stepTargets.forEach((el, i) => {
+            console.log(el, i);
+            el.classList.toggle('upload-step--current', i === this.step);
+        })
+    }
+
+    replaySelected() {
+        this.nextStep();
+        this.uploadFormTarget.submit();
+
+    }
+
+    uploadClicked() {
+        this.replayInputTarget.click();
+    }
+
+    nextStep() {
+        this.step++;
+    }
+
+    prevStep() {
+        this.step--;
+    }
+
+    get step() {
+        return parseInt(this.data.get('step'));
+    }
+
+    set step(i) {
+        this.data.set('step', i.toString());
+        this.showCurrentStep();
     }
 
     openModal() {
-        //this.modal.open();
         this.dialog.open();
-        console.log("Open modal");
     }
 }
