eb023fec8a892ba7ab2f3e59035a22e8de9a460e
Josh LeBlanc <jleblanc@omics.com>
2019-01-23 07:56:42 -0400

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root to: "game_players#index"
-  get 'replay/accuse/:game_id', to: "replay#show_accusation_form"
-  post 'replay/accuse/:game_id', to: "replay#accuse"
-  post 'replay/create'
+  get 'games/:id/accuse', to: "games#accuse", as: :accuse
+  post 'games/:id/accuse', to: "games#update", as: :update_game
+  post 'games/upload', to: "games#upload", as: :upload_game
 
   resources :players, only: [:index, :show]
   resources :games, only: [:show, :index]
-  # get 'players', controller: :players, action: :index
-  # get 'players/:id/games', controller: :players, action: :games
-  # get 'players/:id/game/:game_id', controller: :players, action: :game
-  post "/games/:id/accuse", to: "games#accusation", as: :accuse_game_player
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
