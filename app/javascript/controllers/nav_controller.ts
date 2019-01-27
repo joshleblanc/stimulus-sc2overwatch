bd00ec1ebbec33493e3edd70333e019d374b1616
Joshua LeBlanc <me@jleb.ca>
2019-01-27 11:18:31 -0400

    refactored search into its own controller

--- app/javascript/controllers/nav_controller.ts
+++ app/javascript/controllers/nav_controller.ts
@@ -2,25 +2,20 @@ import {Controller} from 'stimulus';
 import {drawer, list, ripple} from 'material-components-web/index';
 
 export default class extends Controller {
-    static targets = [ 'drawer', 'list', 'navbar', 'search', 'searchInput', 'step', 'searchResultList', 'searchResultsContainer' ];
+    static targets = [ 'drawer', 'list', 'navbar', 'search' ];
     readonly drawerTarget: HTMLElement;
     readonly listTarget: HTMLElement;
     readonly navbarTarget: HTMLElement;
     readonly searchTarget: HTMLElement;
-    readonly searchInputTarget: HTMLInputElement;
     readonly stepTargets: Array<HTMLElement>;
-    readonly searchResultListTarget: HTMLElement;
-    readonly hasSearchResultListTarget: boolean;
-    readonly searchResultsContainerTarget: HTMLElement;
     drawer: drawer.MDCDrawer;
-    searchTimeout: number;
 
     initialize() {
         this.drawer = new drawer.MDCDrawer(this.drawerTarget);
         const l = new list.MDCList(this.listTarget);
         l.wrapFocus = true;
         this.drawer.listen('MDCDrawer:closed', () => {
-            this.open = false;
+            this.drawerOpen = false;
         });
         this.drawer.list_.singleSelection = true;
         this.showNavbar();
@@ -32,41 +27,13 @@ export default class extends Controller {
         })
     }
 
-    showCurrentStep() {
-        this.stepTargets.forEach((s, i) => {
-            s.classList.toggle('hide', this.step !== i);
-        });
-    }
-
-    search = async () => {
-        const query = this.searchInputTarget.value;
-        this.step = 1;
-        const resp = await fetch(`${this.data.get('searchUrl')}?q=${query}`);
-        this.stepTargets[2].innerHTML = await resp.text();
-        if(this.hasSearchResultListTarget) {
-            const tmp = list.MDCList.attachTo(this.searchResultListTarget);
-            tmp.listElements.forEach(l => new ripple.MDCRipple(l));
-        }
-        this.step = 2;
-    };
-
     itemClicked() {
-        this.open = false;
-    }
-
-    searchChange(e) {
-        clearTimeout(this.searchTimeout);
-        if(e.target.value.length === 0) {
-            this.step = 0;
-        } else {
-            this.searchTimeout = setTimeout(this.search, 500);
-        }
+        this.drawerOpen = false;
     }
 
     showNavbar() {
         this.navbarTarget.classList.toggle('hide', this.searchOpen);
         this.searchTarget.classList.toggle('hide', !this.searchOpen);
-        this.searchResultsContainerTarget.classList.toggle('hide', !this.searchOpen);
     }
 
     toggleSearch() {
@@ -74,10 +41,10 @@ export default class extends Controller {
     }
 
     setDrawerState() {
-        this.drawer.open = this.open;
+        this.drawer.open = this.drawerOpen;
     }
 
-    get open() {
+    get drawerOpen() {
         return this.data.get('open') === "true";
     }
 
@@ -90,21 +57,12 @@ export default class extends Controller {
         this.showNavbar();
     }
 
-    set open(state) {
+    set drawerOpen(state) {
         this.data.set('open', state.toString());
         this.setDrawerState();
     }
 
-    get step() {
-        return parseInt(this.data.get('step'));
-    }
-
-    set step(step) {
-        this.data.set('step', step.toString());
-        this.showCurrentStep();
-    }
-
     toggleDrawer() {
-        this.open = !this.open;
+        this.drawerOpen = !this.drawerOpen;
     }
 }
--- app/javascript/controllers/search_controller.ts
+++ app/javascript/controllers/search_controller.ts
@@ -1,14 +1,18 @@
 import { Controller } from 'stimulus';
+import { list, ripple } from 'material-components-web/index';
 
 export default class extends Controller {
-    static targets = [ 'searchInput', 'step'  ];
+    static targets = [ 'searchInput', 'step', 'searchResultList' ];
     readonly searchInputTarget: HTMLInputElement;
     readonly stepTargets: Array<HTMLElement>;
+    readonly searchResultListTarget: HTMLElement;
+    readonly hasSearchResultListTarget: boolean;
+    searchTimeout: number;
 
     search = async () => {
         const query = this.searchInputTarget.value;
         this.step = 1;
-        const resp = await fetch(`${this.data.get('searchUrl')}?q=${query}`);
+        const resp = await fetch(`${this.data.get('url')}?q=${query}`);
         this.stepTargets[2].innerHTML = await resp.text();
         if(this.hasSearchResultListTarget) {
             const tmp = list.MDCList.attachTo(this.searchResultListTarget);
@@ -17,6 +21,13 @@ export default class extends Controller {
         this.step = 2;
     };
 
+    showCurrentStep() {
+        this.stepTargets.forEach((s, i) => {
+            s.classList.toggle('hide', this.step !== i);
+        });
+    }
+
+
     searchChange(e) {
         clearTimeout(this.searchTimeout);
         if(e.target.value.length === 0) {
@@ -24,5 +35,14 @@ export default class extends Controller {
         } else {
             this.searchTimeout = setTimeout(this.search, 500);
         }
+    }
+
+    get step() {
+        return parseInt(this.data.get('step'));
+    }
+
+    set step(step) {
+        this.data.set('step', step.toString());
+        this.showCurrentStep();
     }
 }
