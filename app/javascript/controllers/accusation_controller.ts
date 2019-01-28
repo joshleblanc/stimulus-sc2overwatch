a3b65ca8e98735ed2950432c57f519bebd0e1427
Josh LeBlanc <jleblanc@omics.com>
2019-01-27 22:13:08 -0400

    wip

--- app/javascript/controllers/accusation_controller.ts
+++ app/javascript/controllers/accusation_controller.ts
@@ -1,16 +1,48 @@
 import { Controller } from 'stimulus';
-import { select, formField } from 'material-components-web/index';
+import { select, textField } from 'material-components-web/index';
 
 export default class extends Controller {
-    static targets = [ "form", "playerSelect" ];
+    static targets = [ "form", "playerSelect", "evidence", "submitButton" ];
 
     readonly formTarget: HTMLFormElement;
     readonly formFieldTarget: HTMLDivElement;
     readonly playerSelectTarget: HTMLDivElement;
+    readonly evidenceTarget: HTMLTextAreaElement;
+    readonly submitButtonTarget: HTMLButtonElement;
     select: select.MDCSelect;
+    evidence: textField.MDCTextField;
+
 
     connect() {
         this.select = new select.MDCSelect(this.playerSelectTarget);
+        this.evidence = new textField.MDCTextField(this.evidenceTarget);
+        this.setSubmitButtonState();
+    }
+
+    setSubmitButtonState() {
+        this.submitButtonTarget.disabled = !this.formValid;
+    }
+
+    setFormValid() {
+        this.formValid = this.select.value.length > 0 && this.evidence.value.length > 0;
+        this.setSubmitButtonState();
+    }
+
+    playerChanged() {
+        this.setFormValid();
+    }
+
+    evidenceChanged() {
+        this.setFormValid();
+        console.log("Evidence Changed");
+    }
+
+    get formValid() {
+        return this.data.get('formValid') === "true";
+    }
+
+    set formValid(b) {
+        this.data.set('formValid', b.toString());
     }
 
 
--- app/javascript/controllers/vote_controller.ts
+++ app/javascript/controllers/vote_controller.ts
@@ -1,8 +1,42 @@
 import { Controller } from 'stimulus';
+import { select } from 'material-components-web/index';
 
 export default class extends Controller {
     static targets = [ "player", "verdict", "submit" ];
     readonly playerTarget: HTMLSelectElement;
     readonly submitTarget: HTMLButtonElement;
     readonly verdictTarget: HTMLSelectElement;
+    player: select.MDCSelect;
+    verdict: select.MDCSelect;
+
+    initialize() {
+        this.player = new select.MDCSelect(this.playerTarget);
+        this.verdict = new select.MDCSelect(this.verdictTarget);
+        this.setSubmitButtonState();
+    }
+
+    setSubmitButtonState() {
+        this.submitTarget.disabled = !this.formValid;
+    }
+
+    setFormValid() {
+        this.formValid = this.player.value.length > 0 && this.verdict.value.length > 0;
+        this.setSubmitButtonState();
+    }
+
+    playerChanged() {
+        this.setFormValid();
+    }
+
+    verdictChanged() {
+        this.setFormValid();
+    }
+
+    set formValid(b) {
+        this.data.set('formValid', b.toString());
+    }
+
+    get formValid() {
+        return this.data.get('formValid') === 'true';
+    }
 }
