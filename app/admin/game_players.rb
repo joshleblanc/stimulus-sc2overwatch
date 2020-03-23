29799b7450784f9bf24ed9443b762971fe415b93
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-03-23 11:45:40 -0300

    add game players to active admin

---
+++ app/admin/game_players.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register GamePlayer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :is_accused, :winner, :evidence, :clan, :race, :mmr, :division, :server_rank, :global_rank, :apm, :team, :color, :guilty_count, :innocent_count, :game_id, :player_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:is_accused, :winner, :evidence, :clan, :race, :mmr, :division, :server_rank, :global_rank, :apm, :team, :color, :guilty_count, :innocent_count, :game_id, :player_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
