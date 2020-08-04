c115a0e9e7fbe63dfe6a0f89921b862ba030b0eb
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-08-04 08:31:02 -0300

    Revert "fix: use cdn in prod"
    This reverts commit 37545995

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  config.active_storage.default_delivery_method = :proxy
-  config.active_storage.resolve_model_to_route = :cdn_proxy
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
