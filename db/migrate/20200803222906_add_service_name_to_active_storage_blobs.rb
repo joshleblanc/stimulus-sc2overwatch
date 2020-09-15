bc8f3d8cfb39f42c7c1b71cca68d6eec63aeb07c
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-09-15 08:09:40 -0300

    fix migration version

--- db/migrate/20200803222906_add_service_name_to_active_storage_blobs.rb
+++ db/migrate/20200803222906_add_service_name_to_active_storage_blobs.rb
@@ -1,4 +1,4 @@
-class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.1]
+class AddServiceNameToActiveStorageBlobs < ActiveRecord::Migration[6.0]
   def up
     unless column_exists?(:active_storage_blobs, :service_name)
       add_column :active_storage_blobs, :service_name, :string
